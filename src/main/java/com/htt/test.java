8ded0997fb9fc6fb2c8cb8faab8a519956045d99
DoublePeach <2389242270@qq.com>
2022-08-09 11:03:01 +0800

    second commit by idea !!!

--- src/main/java/com/htt/test.java
+++ src/main/java/com/htt/test.java
@@ -3,5 +3,6 @@ package com.htt;
 public class test {
     public static void main(String[] args) {
         System.out.println("Git commit is success !!!");
+        System.out.println("Git commit is success2 !!!");
     }
 }
